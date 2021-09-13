014ed840774905e1afad949e7e8bbc6a090e8919
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2021-09-13 14:46:27 +0530

    upload new files

--- Calculators/Home/urls.py
+++ Calculators/Home/urls.py
@@ -5,5 +5,9 @@ from Home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='HomePage')
+    path('', views.index, name='HomePage'),
+    path('mon_inv', views.monthly_investment, name='Monthly Investment'),
+    path('vis_time', views.vis_time, name='Vision Times'),
+    path('step_up_mon_inv', views.step_up_mon_inv, name='Step up Monthly investment'),
+    path('vis_time_up', views.step_up_mon_inv, name='Vision Time(Step-up)'),
 ]
--- Calculators/Home/views.py
+++ Calculators/Home/views.py
@@ -1,6 +1,20 @@
+from django.http import request
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Test')
-    
+    return render(request, 'home.html')
+
+
+def monthly_investment(request):
+    return render(request, 'monthly_investment.html')
+
+def vis_time(request):
+    return render(request, 'vis_time.html')
+
+def step_up_mon_inv(request):
+    return render(request, 'vis_time.html')
+
+def vis_time_up(request):
+    return render(request, 'vis_time.html')
+
