82cd5d3a457c90c5dc31bd9ae4de5ede84283678
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2021-09-14 15:03:03 +0530

    delete model

--- Calculators/Home/admin.py
+++ Calculators/Home/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from Home.models import MonthlyInv
 
 # Register your models here.
-admin.site.register(MonthlyInv)
--- Calculators/Home/models.py
+++ Calculators/Home/models.py
@@ -2,8 +2,4 @@ from Home.views import monthly_investment
 from django.db import models
 
 # Create your models here.
-class MonthlyInv(models.Model):
-    monthly_investment = models.CharField(max_length=30)
-    years = models.CharField(max_length=30)
-    exp_ret = models.CharField(max_length=30)
 
